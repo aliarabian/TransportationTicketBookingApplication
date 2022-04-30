58ee510d820a7fe54bc50a85aff0bf899906fec6
Ali Arabian <aliarabian.dev@gmail.com>
2022-05-01 00:42:07 +0430

    Removes old passenger mapper and its implementation

--- src/main/java/com/platform/business/service/booking/PassengerMapper.java
+++
@@ -1,9 +0,0 @@
-package com.platform.business.service.booking;
-
-import com.platform.business.enitity.PlanePassenger;
-import com.platform.business.service.booking.dto.PlanePassengerDto;
-
-public interface PassengerMapper {
-    PlanePassenger mapToPlanePassenger(PlanePassengerDto passengerDto);
-    PlanePassengerDto mapToPlanePassengerDto(PlanePassenger passenger);
-}
