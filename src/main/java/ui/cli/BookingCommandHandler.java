093d0f3a6138a92b6f3e989ab347d1077185383a
Ali Arabian <aliarabian.dev@gmail.com>
2022-05-01 00:34:35 +0430

    [Refactoring] removes unused PrintWriter

--- src/main/java/ui/cli/BookingCommandHandler.java
+++ src/main/java/ui/cli/BookingCommandHandler.java
@@ -1,19 +1,21 @@
 package ui.cli;
 
-import com.platform.business.enitity.AirlineTransportation;
-import com.platform.business.enitity.PlaneTicket;
-import com.platform.business.enitity.SeatingSection;
+import com.platform.business.mapper.PassengerMapper;
+import com.platform.business.mapper.PlaneTicketMapper;
 import com.platform.business.service.booking.BookingService;
-import com.platform.business.service.booking.PassengerMapperImpl;
+import com.platform.business.service.booking.PlainTicketBookingResource;
 import com.platform.business.service.booking.PlaneTicketBookingService;
-import com.platform.business.service.booking.dto.PlaneBookingPassengerDetail;
-import com.platform.business.service.booking.dto.PlanePassengerDto;
-import com.platform.business.service.booking.dto.PlaneTicketBookingRequest;
+import com.platform.business.service.booking.dto.request.PlaneBookingPassengerDetail;
+import com.platform.business.service.booking.dto.request.PlanePassengerDto;
+import com.platform.business.service.booking.dto.request.PlaneTicketBookingRequest;
+import com.platform.business.service.booking.dto.response.PlaneTicketDto;
 import com.platform.business.service.search.transportations.AirlineTransportationsResource;
+import com.platform.business.service.search.transportations.dto.AirlineTransportationDto;
+import com.platform.business.service.search.transportations.dto.SeatingSectionDto;
+import com.platform.repository.country.InMemoryCountryDao;
 import com.platform.repository.customer.InMemoryCustomerDao;
 import com.platform.repository.transportation.InMemoryAirlineTransportationDao;
 
-import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -24,49 +26,54 @@ import static java.lang.System.*;
 public class BookingCommandHandler implements CommandHandler {
 
     private final AirlineTransportationsResource transportationsResource;
-    private final BookingService bookingService;
+    private final PlainTicketBookingResource bookingResource;
 
     public BookingCommandHandler() {
         transportationsResource = new AirlineTransportationsResource();
-        bookingService = new PlaneTicketBookingService(new InMemoryAirlineTransportationDao(), new InMemoryCustomerDao(), new PassengerMapperImpl());
+        BookingService bookingService = new PlaneTicketBookingService(new InMemoryAirlineTransportationDao(),
+                new InMemoryCustomerDao(),
+                new PassengerMapper(new InMemoryCountryDao()),
+                new PlaneTicketMapper());
+        bookingResource = new PlainTicketBookingResource(bookingService);
     }
 
     @Override
     public void handle() {
-        try (Scanner scanner = new Scanner(in);
-             PrintWriter writer = new PrintWriter(out)) {
+        try (Scanner scanner = new Scanner(in)) {
             out.print("Transportation ID: ");
-            writer.flush();
             try {
                 long transportationId = scanner.nextLong();
-                AirlineTransportation transportation = transportationsResource.getTransportationById(transportationId);
-                printFlightDetails(writer, transportation);
-                writer.flush();
+                AirlineTransportationDto transportation = transportationsResource.getTransportationById(transportationId);
+                printFlightDetails(transportation);
                 out.println();
                 out.print("Enter Section ID: ");
                 long sectionId = scanner.nextLong();
-                SeatingSection section = getSection(transportation, sectionId);
+
+                Optional<SeatingSectionDto> sectionDto = getSection(transportation, sectionId);
+                if (!sectionDto.isPresent()) {
+                    err.println("Wrong Section Id");
+                    return;
+                }
+                SeatingSectionDto section = sectionDto.get();
                 out.println(printableSectionData(section));
                 out.print("Enter Number of Passengers: ");
                 int numberOfPassengers = scanner.nextInt();
                 Set<PlaneBookingPassengerDetail> passengerDetails = getPlaneBookingPassengerDetails(scanner, section, numberOfPassengers);
-
                 PlaneTicketBookingRequest request = new PlaneTicketBookingRequest();
                 request.setCustomerId(924427L);
                 request.setTransportationId(transportationId);
                 request.setSeatingSectionId(sectionId);
                 request.setPassengersBookingDetails(passengerDetails);
-                Set<PlaneTicket> planeTickets = bookingService.bookTickets(request);
+                Set<PlaneTicketDto> planeTickets = bookingResource.bookTickets(request);
                 out.println(planeTickets);
             } catch (InputMismatchException inputMismatchException) {
-                err.println("Invalid Transportation Id");
-                exit(-1);
+                err.println("Invalid Input Type");
             }
 
         }
     }
 
-    private Set<PlaneBookingPassengerDetail> getPlaneBookingPassengerDetails(Scanner scanner, SeatingSection section, int numberOfPassengers) {
+    private Set<PlaneBookingPassengerDetail> getPlaneBookingPassengerDetails(Scanner scanner, SeatingSectionDto section, int numberOfPassengers) {
         Set<PlaneBookingPassengerDetail> passengerDetails = new HashSet<>();
         for (int i = 1; i <= numberOfPassengers; i++) {
             out.println("Passenger #" + i);
@@ -77,6 +84,8 @@ public class BookingCommandHandler implements CommandHandler {
             passengerDto.setLastName(scanner.next());
             out.print("National ID: ");
             passengerDto.setNationalIdNO(scanner.next());
+            out.print("Birthdate: ");
+            passengerDto.setBirthdate(LocalDate.of(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
             out.print("Passport Number: ");
             passengerDto.setPassportNO(scanner.next());
             out.print("Passport Country Code:('IR', 'TR', 'UK', 'US') ");
@@ -94,13 +103,13 @@ public class BookingCommandHandler implements CommandHandler {
         return passengerDetails;
     }
 
-    private Set<Long> getPassengersDesiredPrivileges(Scanner scanner, SeatingSection section) {
+    private Set<Long> getPassengersDesiredPrivileges(Scanner scanner, SeatingSectionDto section) {
         Set<Long> selectedPrivileges = new HashSet<>();
 
-        for (int j = 0; j < section.getSectionPrivileges().size(); j++) {
+        for (int j = 0; j < section.getPrivileges().size(); j++) {
             out.print("Enter Desired Privilege ID: ");
             selectedPrivileges.add(scanner.nextLong());
-            if (j == section.getSectionPrivileges().size() - 1) {
+            if (j == section.getPrivileges().size() - 1) {
                 break;
             }
             out.print("another one?(Y/N) ");
@@ -111,53 +120,53 @@ public class BookingCommandHandler implements CommandHandler {
         return selectedPrivileges;
     }
 
-    private SeatingSection getSection(AirlineTransportation transportation, long sectionId) {
-        return transportation.getVehicle().getSeatingSections().stream()
+    private Optional<SeatingSectionDto> getSection(AirlineTransportationDto transportation, long sectionId) {
+        return transportation.getSections().stream()
                              .filter(seatingSection -> seatingSection.getId().equals(sectionId))
-                             .findFirst().get();
+                             .findFirst();
     }
 
-    private void printFlightDetails(PrintWriter writer, AirlineTransportation transportation) {
-        writer.println("Flight NO: " + transportation.getId());
-        writer.println("Plane Model: " + transportation.getVehicle().getModelName());
-        writer.println("Flight Time: " + transportation.getDeparturesAt().format(DateTimeFormatter.RFC_1123_DATE_TIME));
+    private void printFlightDetails(AirlineTransportationDto transportation) {
+        out.println("Flight NO: " + transportation.getId());
+        out.println("Plane Model: " + transportation.getVehicleModelName());
+        out.println("Flight Time: " + transportation.getDeparturesAt().format(DateTimeFormatter.RFC_1123_DATE_TIME));
         List<String> formattedSectionsInfo = getSectionsInfo(transportation);
-        writer.println("Available Sections: ");
-        formattedSectionsInfo.forEach(writer::write);
+        out.println("Available Sections: ");
+        formattedSectionsInfo.forEach(out::print);
     }
 
-    private List<String> getSectionsInfo(AirlineTransportation transportation) {
-        Set<SeatingSection> seatingSections = transportation.getVehicle()
-                                                            .getSeatingSections();
-        List<SeatingSection> availableSections = seatingSections.stream()
-                                                                .filter(seatingSection -> seatingSection.getAvailableSeats() > 0)
-                                                                .collect(Collectors.toList());
+    private List<String> getSectionsInfo(AirlineTransportationDto transportation) {
+        Set<SeatingSectionDto> seatingSections = transportation
+                .getSections();
+        List<SeatingSectionDto> availableSections = seatingSections.stream()
+                                                                   .filter(seatingSection -> seatingSection.getAvailableSeats() > 0)
+                                                                   .collect(Collectors.toList());
         return availableSections.stream()
                                 .map(this::printableSectionData)
                                 .collect(Collectors.toList());
     }
 
-    private String printableSectionData(SeatingSection section) {
+    private String printableSectionData(SeatingSectionDto section) {
         StringBuilder sb = new StringBuilder();
         sb.append("\tSection ID: ")
           .append(section.getId())
           .append("\n")
           .append("\tSection: ")
-          .append(section.title())
+          .append(section.getTitle())
           .append("\n")
           .append("\tAvailable Seats: ")
           .append(section.getAvailableSeats())
           .append("\n")
           .append("\tPrivileges: \n");
-        if (section.getSectionPrivileges().isEmpty())
+        if (section.getPrivileges().isEmpty())
             sb.append("\t\tNo Privileges\n");
         else {
-            section.getSectionPrivileges().forEach(privilege ->
+            section.getPrivileges().forEach(privilege ->
                     sb.append("\t\tPrivilege Id: ")
                       .append(privilege.getId())
                       .append("\n")
                       .append("\t\tPrivilege: ")
-                      .append(privilege.getServiceDescription())
+                      .append(privilege.getDescription())
                       .append("\n")
             );
         }
