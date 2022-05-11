018cd2e86f2110bc235aaf8b6ce54006d7e68125
Arabian Ali <A.Arabian@tosan.com>
2022-05-11 17:40:26 +0430

    Sets response content-type of javascript module files(.mjs) to text/javascript

--- src/main/java/ui/servlet/filter/StaticResourceAccessFilter.java
+++ src/main/java/ui/servlet/filter/StaticResourceAccessFilter.java
@@ -1,6 +1,7 @@
 package ui.servlet.filter;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -13,9 +14,12 @@ public class StaticResourceAccessFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         if (req.getRequestURI().substring(req.getContextPath().length()).startsWith("/static/")) {
+            if (req.getRequestURI().endsWith(".mjs")) {
+                res.setContentType("text/javascript");
+            }
             getServletContext().getNamedDispatcher("default").forward(req, res);
             return;
         }
-        chain.doFilter(req,res);
+        chain.doFilter(req, res);
     }
 }
