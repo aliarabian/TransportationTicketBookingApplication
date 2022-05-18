2ca378c0617fb8d18eafdf3916b640e494e1005c
aliarabian <aliarabian.dev@gmail.com>
2022-05-18 15:49:47 +0430

    [Refactoring] chnages response type to Spring's ResponseEntity

--- src/main/java/com/platform/controllers/auth/AuthenticationController.java
+++ src/main/java/com/platform/controllers/auth/AuthenticationController.java
@@ -7,9 +7,14 @@ import com.platform.business.auth.AuthenticationRequest;
 import com.platform.business.auth.AuthenticationResponse;
 import com.platform.business.auth.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "auth", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
@@ -18,11 +23,13 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
-    public ApiResponseEntity<AuthenticationResponse> login(AuthenticationRequest request) {
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponseEntity<AuthenticationResponse>> login(AuthenticationRequest request) {
         try {
-            return new ApiResponseEntity<>(authenticationService.authenticate(request), false);
+            return ResponseEntity.ok(new ApiResponseEntity<>(authenticationService.authenticate(request)));
         } catch (ApplicationException exception) {
-            return new ApiResponseEntity<>(new ApiErrorResponse(exception.getMessage(), exception.errorCode()) );
+            return ResponseEntity.ok(new ApiResponseEntity<>(new ApiErrorResponse(exception.getMessage(), exception.errorCode())));
+
         }
     }
 
