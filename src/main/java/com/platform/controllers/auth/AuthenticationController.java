c638ffd8a162356a63f903039a60d8982f0a165b
aliarabian <aliarabian.dev@gmail.com>
2022-05-23 15:59:39 +0430

    Implements JwtDecoder FunctionalInterface with jose4j api

--- src/main/java/com/platform/controllers/auth/AuthenticationController.java
+++ src/main/java/com/platform/controllers/auth/AuthenticationController.java
@@ -2,31 +2,19 @@ package com.platform.controllers.auth;
 
 import com.platform.ApiErrorResponse;
 import com.platform.ApiResponseEntity;
-import com.platform.business.exception.ApplicationException;
 import com.platform.business.service.auth.AuthenticationRequest;
 import com.platform.business.service.auth.AuthenticationResponse;
 import com.platform.business.service.auth.AuthenticationService;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping(value = "auth", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 public class AuthenticationController {
@@ -43,7 +31,6 @@ public class AuthenticationController {
         try {
             AuthenticationResponse authenticationResponse = authenticationService.authenticate(request);
             return ResponseEntity.ok(new ApiResponseEntity<>(authenticationResponse));
-
         } catch (AuthenticationException authenticationException) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
                     new ApiResponseEntity<>(new ApiErrorResponse(authenticationException.getMessage(), 14021)));
